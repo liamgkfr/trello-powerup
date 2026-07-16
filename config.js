39529fb8e22fb5c5eefdd7c2aafe03a640634312
liamgkfr <liam.gangkofer@sg-cnc.de>
2026-07-16 08:42:06 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 // Wichtig: Dieses Objekt darf nicht mit Object.freeze() gesperrt werden,
 // da die Trello-Bibliothek interne Optionen daran ergaenzt.
 window.POWER_UP_CONFIG = {
-  appKey: 'HIER_DEINEN_TRELLO_API_KEY_EINTRAGEN',
+  appKey: 'ddc73abd32cbff2afd4a0dc82dbbbead',
   appName: 'SG Eingabe',
   appAuthor: 'SG CNC'
 };
