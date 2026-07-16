857da3ef2629e1de5fd97af8f6f5753cd785da56
liamgkfr <liam.gangkofer@sg-cnc.de>
2026-07-16 08:10:21 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 // Trage hier den API-Schluessel deines Trello-Power-Ups ein.
 // Trello: Power-Up-Verwaltung -> API Key -> Generate a new API Key
 window.POWER_UP_CONFIG = Object.freeze({
-  appKey: 'HIER_DEINEN_TRELLO_API_KEY_EINTRAGEN',
+  appKey: 'ddc73abd32cbff2afd4a0dc82dbbbead',
   appName: 'SG Eingabe',
   appAuthor: 'SG CNC'
 });
